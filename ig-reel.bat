e7f5ca08f1bc5ce4f858ee9e853be423b168b7a4
eric-k-chu <kiyuen88ec@gmail.com>
2024-10-07 20:23:18 -0700

    added ffmpeg

--- ig-reel.bat
+++ ig-reel.bat
@@ -6,6 +6,6 @@ set /p "res=Which browser are you logged into Instagram on (chrome/firefox): "
 
 echo Downloading Instagram reel from: %url%
 
-yt-dlp %url% -S res:720 --write-info-json -o "%%(id)s" --cookies-from-browser %browser%
+yt-dlp %url% -S res:720 --write-info-json -o "%%(id)s" --cookies-from-browser %browser% --ffmpeg-location "./ffmpeg/bin/ffmpeg.exe"
 
 pause
--- yt-playlist-channel.sh
+++ yt-playlist-channel.sh
@@ -4,4 +4,4 @@ read -p "Enter the URL of the YouTube playlist: " url
 
 echo "Downloading YouTube playlist/channel from: $url"
 
-yt-dlp $url -S res:720 --write-info-json -o "%(playlist)s/%(id)s"
+yt-dlp $url -S res:720 --write-info-json -o "%(playlist)s/%(id)s" --ffmpeg-location "./ffmpeg/bin/ffmpeg.exe"
