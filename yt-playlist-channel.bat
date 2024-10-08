a739e971f068eae089d235ff9a92cb745719b078
eric-k-chu <kiyuen88ec@gmail.com>
2024-10-07 20:34:47 -0700

    fixed ffmpeg path

--- yt-playlist-channel.bat
+++ yt-playlist-channel.bat
@@ -4,6 +4,6 @@ set /p "url=Enter the URL of the Youtube playlist or channel: "
 
 echo Downloading Youtube playlist or channel from: %url%
 
-yt-dlp %url% -S res:720 --write-info-json -o "%%(playlist)s/%%(id)s" --ffmpeg-location "./ffmpeg/bin/ffmpeg.exe"
+yt-dlp %url% -S res:720 --write-info-json -o "%%(playlist)s/%%(id)s" --ffmpeg-location ".\ffmpeg\bin"
 
 pause
--- yt-video.sh
+++ yt-video.sh
@@ -4,4 +4,4 @@ read -p "Enter the URL of the YouTube video: " url
 
 echo "Downloading YouTube video from: $url"
 
-yt-dlp $url -S res:720 --write-info-json -o "%(id)s" --ffmpeg-location "./ffmpeg/bin/ffmpeg.exe"
+yt-dlp $url -S res:720 --write-info-json -o "%(id)s" --ffmpeg-location "./ffmpeg/bin"
