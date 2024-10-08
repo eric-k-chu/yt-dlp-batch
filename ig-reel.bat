85b388bdefc7ea76027b9d36df94eaead76a5a93
Eric Chu <eric@terros.com>
2024-10-08 14:34:20 -0700

    added ffmpeg location and fixed ig reel browser

--- ig-reel.bat
+++ ig-reel.bat
@@ -2,10 +2,12 @@
 
 set /p "url=Enter the URL of the Instagram reel: "
 
-set /p "res=Which browser are you logged into Instagram on (chrome/firefox): "
+set "browser=chrome"
+
+set /p "Which browser are you logged into Instagram on (chrome/firefox) default is chrome: "
 
 echo Downloading Instagram reel from: %url%
 
-yt-dlp %url% -S res:720 --write-info-json -o "%%(id)s" --cookies-from-browser %browser%
+yt-dlp %url% -P "output" -S res:720 --write-info-json -o "%%(id)s" --cookies-from-browser %browser% --ffmpeg-location ".\ffmpeg\bin"
 
 pause
--- yt-video.bat
+++ yt-video.bat
@@ -4,6 +4,6 @@ set /p "url=Enter the URL of the YouTube video: "
 
 echo Downloading YouTube video from: %url%
 
-yt-dlp %url% -S res:720 --write-info-json -o "%%(id)s"
+yt-dlp %url% -P "output" -S res:720 --write-info-json -o "%%(id)s" --ffmpeg-location ".\ffmpeg\bin"
 
 pause
